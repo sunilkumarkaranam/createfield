c3ef0573f9f90bffdf4e9bd037e470fdb03e1d2c
Sunil Kumar K (MS/ETB72-PS) <uql1kor@bosch.com>
2022-09-20 17:20:59 +0530

    bghriejygt

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,20 @@ import ForgeUI, { render, Text, Fragment, GlobalPage } from '@forge/ui';
 const App = () => {
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Button text="Create Custom field" onClick={() => setOpen(true)} />
+      {isOpen && (
+        <ModalDialog header="New custom field" onClose={() => setOpen(false)}>
+          
+          <Form onSubmit={onSubmit} submitButtonText="Add">
+         
+         
+            <TextField label="Key" name="fieldkey" />
+            <TextField label="Type" name="fieldtype" />
+            <TextField label="Name" name="fieldname" />
+            
+          </Form>
+        </ModalDialog>
+      )}
     </Fragment>
   );
 };
